2d128d0fac5f72adf63a0c05bc0366db131a0842
Ma. Cristina Gonzalez Mayorga <mcrisgm@Mac-mini-de-Ma-Cristina-Gonzalez-Mayorga.local>
2013-10-10 19:28:16 -0500

    Initial Commit

---
+++ kupay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kupay
+//
+//  Created by Ma. Cristina Gonzalez Mayorga on 10/10/13.
+//  Copyright (c) 2013 ku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
