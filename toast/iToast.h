833295ba32dedbac8d547ddb2256281e5a761533
Codigo KU <KU@Mac-mini-de-Codigo.local>
2014-05-14 15:20:02 -0500

    arreglando problemas de inclucion de toas en repo

---
+++ toast/iToast.h
@@ -0,0 +1,84 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef enum iToastGravity {
+	iToastGravityTop = 1000001,
+	iToastGravityBottom,
+	iToastGravityCenter
+}iToastGravity;
+
+enum iToastDuration {
+	iToastDurationLong = 10000,
+	iToastDurationShort = 1000,
+	iToastDurationNormal = 3000
+}iToastDuration;
+
+typedef enum iToastType {
+	iToastTypeInfo = -100000,
+	iToastTypeNotice,
+	iToastTypeWarning,
+	iToastTypeError,
+	iToastTypeNone // For internal use only (to force no image)
+}iToastType;
+
+typedef enum {
+    iToastImageLocationTop,
+    iToastImageLocationLeft
+} iToastImageLocation;
+
+
+@class iToastSettings;
+
+@interface iToast : NSObject {
+	iToastSettings *_settings;
+	NSInteger offsetLeft;
+	NSInteger offsetTop;
+	
+	NSTimer *timer;
+	
+	UIView *view;
+	NSString *text;
+}
+
+- (void) show;
+- (void) show:(iToastType) type;
+- (iToast *) setDuration:(NSInteger ) duration;
+- (iToast *) setGravity:(iToastGravity) gravity 
+			 offsetLeft:(NSInteger) left
+			 offsetTop:(NSInteger) top;
+- (iToast *) setGravity:(iToastGravity) gravity;
+- (iToast *) setPostion:(CGPoint) position;
+
++ (iToast *) makeText:(NSString *) text;
+
+-(iToastSettings *) theSettings;
+
+@end
+
+
+
+@interface iToastSettings : NSObject<NSCopying>{
+	NSInteger duration;
+	iToastGravity gravity;
+	CGPoint postition;
+	iToastType toastType;
+	
+	NSDictionary *images;
+	
+	BOOL positionIsSet;
+}
+
+
+@property(assign) NSInteger duration;
+@property(assign) iToastGravity gravity;
+@property(assign) CGPoint postition;
+@property(readonly) NSDictionary *images;
+@property(assign) iToastImageLocation imageLocation;
+
+
+- (void) setImage:(UIImage *)img forType:(iToastType) type;
+- (void) setImage:(UIImage *)img withLocation:(iToastImageLocation)location forType:(iToastType)type;
++ (iToastSettings *) getSharedSettings;
+						  
+@end
